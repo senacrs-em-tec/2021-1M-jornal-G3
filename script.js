115351a3d44384cef5744c1fe8480c6f40ca0a03
evellynlwt <yanaluiza26y@gmail.com>
2021-12-16 10:00:37 -0300

    Commit Jornal

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
   } else {
-    x.style.display = "block";
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
   }
 }
 
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
